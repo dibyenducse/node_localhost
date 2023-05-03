694b17048ef264f2352f57d291951c735abef401
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-05-04 03:55:46 +0600

    self invoke function and closure

--- myscript.js
+++ myscript.js
@@ -1,1 +1,28 @@
+const add = (function () {
+    let counter = 0
+    return function () {
+        counter += 1
+        return counter
+    }
+})()
 
+const d = add
+
+console.log(d())
+console.dir(d)
+
+const add2 = function () {
+    let counter = 0
+    return function () {
+        counter += 1
+        return counter
+    }
+}
+
+const myFunc = add2()
+
+console.log(myFunc())
+console.log(myFunc())
+console.log(myFunc())
+console.log(myFunc())
+console.dir(myFunc)
