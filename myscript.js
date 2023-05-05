da097e1fad28fed174378af6e12d1b8cc6163e94
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-05-05 23:38:11 +0600

    Restaurent Order Process

--- myscript.js
+++ myscript.js
@@ -1,77 +1,13 @@
-// const add = (function () {
-//     let counter = 0
-//     return function () {
-//         counter += 1
-//         return counter
-//     }
-// })()
+const processOrder = (customer) => {
+    console.log('processing order for customer 1')
 
-// const d = add
+    var currentTime = new Date().getTime()
 
-// console.log(d())
-// console.dir(d)
+    while (currentTime + 3000 >= new Date().getTime());
 
-// const add2 = function () {
-//     let counter = 0
-//     return function () {
-//         counter += 1
-//         return counter
-//     }
-// }
-
-// const myFunc = add2()
-
-// console.log(myFunc())
-// console.log(myFunc())
-// console.log(myFunc())
-// console.log(myFunc())
-// console.dir(myFunc)
-
-// let person = {
-//     name: 'dibbo',
-// }
-
-// let person2 = {
-//     name: 'sagor',
-// }
-// let getAge = () => {}
-
-// console.log(Function.prototype == Object.prototype)
-
-// Object
-// Boolean
-// Function
-// String
-// Number
-
-// // Object.hasOwnProperty()
-// // Object.getPrototypeOf()
-// // Object.hasOwnProperty()
-
-// let obj = Object.create(null)
-// let obj2 = {}
-
-// console.log(obj2)
-2
-function sayHello(action) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            for (let i = 0; i < 100000; i++) {
-                console.log(i)
-            }
-            resolve(action())
-        }, 3000)
-    })
+    console.log('order process for customer 1')
 }
 
-// async function myFunc() {
-//     console.log('before')
-//     await
-//     console.log('after')
-// }
-
-sayHello(() => {
-    return 'hello World'
-}).then((res) => {
-    console.log(res)
-})
+console.log('take order for customer 1')
+processOrder()
+console.log('completed order for customer 1')
