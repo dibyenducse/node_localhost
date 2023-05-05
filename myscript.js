46664b3082af1bfdec6ae0e2321ab7a859d0be91
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-05-05 23:13:18 +0600

    async function

--- myscript.js
+++ myscript.js
@@ -1,28 +1,77 @@
-const add = (function () {
-    let counter = 0
-    return function () {
-        counter += 1
-        return counter
-    }
-})()
+// const add = (function () {
+//     let counter = 0
+//     return function () {
+//         counter += 1
+//         return counter
+//     }
+// })()
 
-const d = add
+// const d = add
 
-console.log(d())
-console.dir(d)
+// console.log(d())
+// console.dir(d)
 
-const add2 = function () {
-    let counter = 0
-    return function () {
-        counter += 1
-        return counter
-    }
+// const add2 = function () {
+//     let counter = 0
+//     return function () {
+//         counter += 1
+//         return counter
+//     }
+// }
+
+// const myFunc = add2()
+
+// console.log(myFunc())
+// console.log(myFunc())
+// console.log(myFunc())
+// console.log(myFunc())
+// console.dir(myFunc)
+
+// let person = {
+//     name: 'dibbo',
+// }
+
+// let person2 = {
+//     name: 'sagor',
+// }
+// let getAge = () => {}
+
+// console.log(Function.prototype == Object.prototype)
+
+// Object
+// Boolean
+// Function
+// String
+// Number
+
+// // Object.hasOwnProperty()
+// // Object.getPrototypeOf()
+// // Object.hasOwnProperty()
+
+// let obj = Object.create(null)
+// let obj2 = {}
+
+// console.log(obj2)
+2
+function sayHello(action) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            for (let i = 0; i < 100000; i++) {
+                console.log(i)
+            }
+            resolve(action())
+        }, 3000)
+    })
 }
 
-const myFunc = add2()
+// async function myFunc() {
+//     console.log('before')
+//     await
+//     console.log('after')
+// }
 
-console.log(myFunc())
-console.log(myFunc())
-console.log(myFunc())
-console.log(myFunc())
-console.dir(myFunc)
+sayHello(() => {
+    return 'hello World'
+}).then((res) => {
+    console.log(res)
+})
