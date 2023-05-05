2ac1a67ee1e8ab0e7c96e693b5599f4b336b30fa
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-05-06 02:13:58 +0600

    async try catch

--- myscript.js
+++ myscript.js
@@ -1,24 +1,51 @@
-const hasMeeting = true
+// const hasMeeting = false
 
-const meeting = new Promise((resolve, reject) => {
-    if (!hasMeeting) {
-        const meetingDetails = {
-            name: 'Technical Meeting',
-            location: 'Google Meet',
-            time: '10:00PM',
-        }
-        resolve(meetingDetails)
-    } else {
-        reject(new Error('Meeting already scheduled'))
-    }
+// const meeting = new Promise((resolve, reject) => {
+//     if (!hasMeeting) {
+//         const meetingDetails = {
+//             name: 'Technical Meeting',
+//             location: 'Google Meet',
+//             time: '10:00PM',
+//         }
+//         resolve(meetingDetails)
+//     } else {
+//         reject(new Error('Meeting already scheduled'))
+//     }
+// })
+
+// const addToCalendar = (meetingDetails) => {
+//     //return new Promise((resolve, reject) => {
+//     const calender = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`
+//     return Promise.resolve(calender)
+//     // })
+// }
+
+// meeting
+//     .then(addToCalendar)
+//     .then((res) => {
+//         console.log(JSON.stringify(res))
+//     })
+//     .catch((err) => {
+//         console.log(err.message)
+//     })
+
+// console.log('hello')
+
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Promise2 resolved')
+    })
 })
 
-meeting
-    .then((res) => {
-        console.log(JSON.stringify(res))
-    })
-    .catch((err) => {
-        console.log(err.message)
-    })
+const promise2 = Promise.resolve('Promise1 resolved')
 
-console.log('hello')
+// promise1.then((res) => console.log(res))
+// promise2.then((res) => console.log(res))
+
+// Promise.all([promise1, promise2]).then((res) => {
+//     console.log(res)
+// })
+
+Promise.race([promise1, promise2]).then((res) => {
+    console.log(res)
+})
