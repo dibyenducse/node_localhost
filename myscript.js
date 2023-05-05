b24605cb9f3442a26f68736009aaa7be341a69c4
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-05-06 00:17:54 +0600

    async callback

--- myscript.js
+++ myscript.js
@@ -1,13 +1,26 @@
-const processOrder = (customer) => {
-    console.log('processing order for customer 1')
+const takeOrder = (customer, callback) => {
+    console.log(`take order for customer 1 ${customer}`)
+    callback(customer)
+}
+
+const processOrder = (customer, callback) => {
+    console.log(`processing order for customer 1 ${customer}`)
 
     setTimeout(() => {
         console.log('cooking completed')
+        console.log(`order process for customer 1 ${customer}`)
+        callback(customer)
     }, 3000)
-
-    console.log('order process for customer 1')
 }
 
-console.log('take order for customer 1')
-processOrder()
-console.log('completed order for customer 1')
+const completeOrder = (customer) => {
+    console.log(`completed order for ${customer}`)
+}
+
+takeOrder('customer 1', (customer) => {
+    processOrder(customer, (customer) => {
+        completeOrder(customer)
+    })
+})
+
+console.log('Hello')
