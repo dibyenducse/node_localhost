51f10fe76b754d2990ce882764bc504f004099e7
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-05-06 00:35:27 +0600

    promise then catch

--- myscript.js
+++ myscript.js
@@ -1,26 +1,22 @@
-const takeOrder = (customer, callback) => {
-    console.log(`take order for customer 1 ${customer}`)
-    callback(customer)
-}
+const hasMeeting = true
 
-const processOrder = (customer, callback) => {
-    console.log(`processing order for customer 1 ${customer}`)
-
-    setTimeout(() => {
-        console.log('cooking completed')
-        console.log(`order process for customer 1 ${customer}`)
-        callback(customer)
-    }, 3000)
-}
-
-const completeOrder = (customer) => {
-    console.log(`completed order for ${customer}`)
-}
-
-takeOrder('customer 1', (customer) => {
-    processOrder(customer, (customer) => {
-        completeOrder(customer)
-    })
+const meeting = new Promise((resolve, reject) => {
+    if (!hasMeeting) {
+        const meetingDetails = {
+            name: 'Technical Meeting',
+            location: 'Google Meet',
+            time: '10:00PM',
+        }
+        resolve(meetingDetails)
+    } else {
+        reject(new Error('Meeting already scheduled'))
+    }
 })
 
-console.log('Hello')
+meeting
+    .then((res) => {
+        console.log(JSON.stringify(res))
+    })
+    .catch((err) => {
+        console.log(err.message)
+    })
